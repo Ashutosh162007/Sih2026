f2928f5087c9a3578b8c4762718a3b90f6847465
meetvartak24 <meetvartak24@gmail.com>
2026-09-05 00:18:41 +0530

    .

--- Backend/routes/industryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProposals, getFundedProjects } = require('../controllers/industryController');
-
-router.get('/proposals', getProposals);
-router.get('/projects', getFundedProjects);
-
-module.exports = router;
